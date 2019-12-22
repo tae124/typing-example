2faf78615da5e54be2e223ff262e49e895bbfd18
tae124 <agnelloxy@gmail.com>
2019-12-22 21:26:57 +0900

    use typeguard

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from typing import NewType
+from typeguard import typechecked
 
 UserId = NewType('UserId', int)
 PostId = NewType('PostId', int)
 
+@typechecked
 def greeting(name: str, userId: UserId) -> str:
-    return 'Hello ' + name + " UserId:{userId}".format(userId=userId)
+  return 'Hello ' + name + " UserId:{userId}".format(userId=userId)
 
-msg = greeting('tae', PostId(10))
+msg = greeting('tae', 'number')
 print(msg)
